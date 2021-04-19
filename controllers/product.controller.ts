60e00c07eeb5337a1dd7b5ff678580432ecd5f9b
Mohamed Habib Alouani <mhabib.alouani@gmail.com>
2021-04-19 16:55:07 +0000

    refactor code add server side pagination

--- controllers/product.controller.ts
+++ controllers/product.controller.ts
@@ -28,8 +28,19 @@ async function getSingleProduct(id: number) {
   return await Product.find({id: id});
 }
 
-async function getAllProduct(query: Object,limit: number) {
-  return await Product.find(query).limit(limit);
+async function getAllProduct(query: Object,limit: number, page: number) {
+  let allData = {data: {}, totalPages: 0, currentPage: 0};
+  if(page === 0) {
+    const Products = Product.find(query).limit(limit);
+    await Products.then(res => allData.data = res);
+  } else {
+    const Data = Product.find(query).limit(limit).skip(limit * (page - 1)).exec();
+    const count = await Product.countDocuments();
+    allData.totalPages = Math.ceil(count / limit);
+    allData.currentPage = page;
+    await Data.then(res => allData.data = res);
+  }
+  return allData
 }
 
 // async function paginitaion(page:number, size:number) {
--- routes/index.ts
+++ routes/index.ts
@@ -3,7 +3,7 @@ import productController from '../controllers/product.controller';
 
 
 export default ({app}: TRoutesInput) => {
-  app.post('/api/newproducts/', async(req, res) => {
+  app.post('/api/newproduct/', async(req, res) => {
     const myProducts = {
         id: req.body.id,
         title: req.body.title,
@@ -18,8 +18,9 @@ export default ({app}: TRoutesInput) => {
 
   app.get('/api/products/:category?', async(req, res) => {
     const limit = Number(req.query.limit) || 0;
+    const page = Number(req.query.page) || 0;
     const query = req.params.category ? {category: req.params.category} : {}
-    await productController.getAllProduct(query, limit)
+    await productController.getAllProduct(query, limit, page)
       .then(products => res.json(products))
       .catch(err => res.status(400).json('Error: ' + err));
   })
